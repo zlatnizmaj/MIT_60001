d59620c7b3e78125d0ccc09d9fe60af4996ea60d
MrGao <decakgao@gmail.com>
2018-06-20 23:53:28 +0700

    modified

--- Lecture codes/Dictionary02_python.py
+++
@@ -1,9 +0,0 @@
-grades = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5 }
-for s in grades: # print all keys of dictionary 'grades'
-
-    print("key '{}' have value: {}".format(s, grades[s]))
-L = grades.values()
-print(len(L))
-print(grades.keys())
-
-print(L)
--- Lecture codes/Function_python.py
+++
@@ -1,7 +0,0 @@
-def f():
-    def x(a, b):
-        return a+b
-    return x
-val = f()(3,4)
-
-print(val)
